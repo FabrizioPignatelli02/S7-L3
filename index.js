bbb2dbc3f80eff76abc628c880682e8a240718f2
Fabrizio Giuseppe Pignatelli <141920930+FabrizioPignatelli02@users.noreply.github.com>
2023-11-08 16:11:31 +0100

    style.ccs card

--- index.js
+++ index.js
@@ -6,5 +6,31 @@ fetch(url)
   .then((obj) => {
     console.log("obj", obj);
 
-    obj.forEach((book) => {});
+    const library = document.getElementById("library");
+
+    obj.forEach((book) => {
+      const divCard = document.createElement("div");
+      divCard.classList.add("card");
+
+      const imgCard = document.createElement("img");
+      imgCard.classList.add("card-img-top");
+      imgCard.src = book.img;
+
+      const divCardBody = document.createElement("div");
+      divCardBody.classList.add("card-body");
+
+      const title = document.createElement("h5");
+      title.classList.add("card-title");
+      title.innerText = book.title;
+
+      const price = document.createElement("p");
+      price.classList.add("card-text");
+      price.innerText = "€ " + book.price;
+
+      divCard.appendChild(imgCard);
+      divCard.appendChild(divCardBody);
+      divCardBody.appendChild(title);
+      divCardBody.appendChild(price);
+      library.appendChild(divCard);
+    });
   });
